c4dd26aab51e3fa9ca2de8ee18b8d3d9022e3f4b
Craig Nuzzo <1403284-cdnuzzo@users.noreply.gitlab.com>
2026-06-26 08:08:24 -0500

    Utilize a JSON file for the meals

--- meal_planner/meals.py
+++ meal_planner/meals.py
@@ -1,34 +1,5 @@
-PRODUCE = "Produce"
-MEATS = "Meats"
-FROZEN = "Frozen"
-OTHER = "Other"
+import json
+from pathlib import Path
 
-MEALS = [
-    {
-        "name": "Taco Salad",
-        "ingredients": [
-            {"name": "ground beef", "category": MEATS},
-            {"name": "SouthWest salad", "category": PRODUCE},
-            {"name": "mexican cheese", "category": OTHER},
-            {"name": "Doritos", "category": OTHER},
-            {"name": "shredded iceberg lettuce", "category": PRODUCE},
-        ]
-    },
-    {
-        "name": "Caesar Salad",
-        "ingredients": [
-            {"name": "Caesar Salad mix", "category": PRODUCE},
-            {"name": "Chicken or Steak", "category": MEATS},
-            {"name": "Croutons", "category": OTHER},
-        ]
-    },
-    {
-        "name": "Ribs",
-        "ingredients": [
-            {"name": "ribs", "category": MEATS},
-            {"name": "carrots", "category": PRODUCE},
-            {"name": "fries", "category": FROZEN},
-            {"name": "BBQ sauce", "category": OTHER},
-        ]
-    }
-]
+with open(Path(__file__).parent / "meals.json") as f:
+    MEALS = json.load(f)
